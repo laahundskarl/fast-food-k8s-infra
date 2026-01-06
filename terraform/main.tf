d1adc1e8b5d69383e5258e98f6b9dae1d2c38a41
WillianBorba <willian.rocha.borba@gmail.com>
2026-01-06 18:10:31 -0300

    fix: update RDS subnet IDs and add payments MySQL instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,11 +9,11 @@ data "aws_vpc" "existing" {
 
 # Subnets específicas para o EKS
 data "aws_subnet" "fastfood_subnet_1a" {
-  id = "subnet-08d34ed68511f3917"  # us-east-1a
+  id = "subnet-0f244c624d019846b"  # us-east-1a
 }
 
 data "aws_subnet" "fastfood_subnet_1b" {
-  id = "subnet-07fe020cefc4bd241"  # us-east-1b
+  id = "subnet-02ec0d1778295e935"  # us-east-1b
 }
 
 # Lista das subnets para o EKS
